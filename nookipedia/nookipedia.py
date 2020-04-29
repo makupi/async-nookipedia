f07d0cb3ddbf755ffd23ee4e91e4d14316801995
makubob <makupi@icloud.com>
2020-04-29 23:44:02 +0200

    Adds Object Endpoints

--- nookipedia/nookipedia.py
+++ nookipedia/nookipedia.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 import aiohttp
 
 from nookipedia.models import Critter, Fossil, Villager
 
 API = "https://nookipedia.com/api"
+
+
+def is_valid(data: dict) -> bool:
+    if "error" in data:
+        return False
+    return True
 
 
 class Nookipedia:
@@ -24,11 +32,20 @@ class Nookipedia:
     async def get_fossil_raw(self, name: str) -> dict:
         return await self._fetch_json(f"{API}/fossil/{name}/")
 
-    async def get_villager(self, name: str) -> Villager:
-        pass
+    async def get_villager(self, name: str) -> Optional[Villager]:
+        data = await self.get_villager_raw(name)
+        if not is_valid(data):
+            return None
+        return Villager(data=data)
 
-    async def get_critter(self, name: str) -> Critter:
-        pass
+    async def get_critter(self, name: str) -> Optional[Critter]:
+        data = await self.get_critter_raw(name)
+        if not is_valid(data):
+            return None
+        return Critter(data=data)
 
-    async def get_fossil(self, name: str) -> Fossil:
-        pass
+    async def get_fossil(self, name: str) -> Optional[Fossil]:
+        data = await self.get_fossil_raw(name)
+        if not is_valid(data):
+            return None
+        return Fossil(data=data)
