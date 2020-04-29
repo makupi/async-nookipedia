2b06ffdf77b889482dd18a3411d380a7a5479bd9
makubob <makupi@icloud.com>
2020-04-29 23:33:43 +0200

    Adds Fossil parsing

--- nookipedia/models/fossil.py
+++ nookipedia/models/fossil.py
@@ -1,3 +1,14 @@
-class Fossil:
-    def __init__(self):
-        pass
+from .cached_object import CachedObject
+
+
+class Fossil(CachedObject):
+    def __init__(self, data: dict):
+        super().__init__(data.get("updated"), data.get("cached"))
+        self.name = data.get("name")
+        self.image = data.get("image")
+        self.scientific_name = data.get("scientific-name")
+        self.sections = data.get("sections")
+        self.period = data.get("period")
+        self.length = data.get("length")
+        self.price = data.get("price")
+        self.link = data.get("link")
