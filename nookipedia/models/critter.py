075f9e8090cc3472f9dfd1a437e1af85bb7280cf
makubob <makupi@icloud.com>
2020-04-29 23:28:32 +0200

    Adds Critter parsing

--- nookipedia/models/critter.py
+++ nookipedia/models/critter.py
@@ -1,3 +1,20 @@
-class Critter:
-    def __init__(self):
-        pass
+from .cached_object import CachedObject
+
+
+class Critter(CachedObject):
+    def __init__(self, data: dict):
+        super().__init__(data.get("updated"), data.get("cached"))
+        self.name = data.get("name")
+        self.image = data.get("image")
+        self.scientific_name = data.get("scientific-name")
+        self.family = data.get("family")
+        self.time_year = data.get("time-year")
+        self.time_day = data.get("time-day")
+        self.location = data.get("location")
+        self.size = data.get("size")
+        self.rarity = data.get("rarity")
+        self.price = data.get("price")
+        self.caught = data.get("caught")
+        self.shadow = data.get("shadow")
+        self.movement = data.get("movement")
+        self.link = data.get("link")
