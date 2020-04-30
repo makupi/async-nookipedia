bb3b4dc916c6f0cfc29aa8e4220d7e98d1fade05
makubob <makupi@icloud.com>
2020-04-30 20:36:53 +0200

    Splits API from Nookipedia class

---
+++ nookipedia/api/api.py
@@ -0,0 +1,22 @@
+import aiohttp
+
+
+class API:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = "https://nookipedia.com/api"
+
+    async def _fetch_json(self, url: str) -> dict:
+        headers = {"X-API-KEY": self.api_key}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=url, headers=headers) as response:
+                return await response.json()
+
+    async def get_villager(self, name: str) -> dict:
+        return await self._fetch_json(f"{self.url}/villager/{name}/")
+
+    async def get_critter(self, name: str) -> dict:
+        return await self._fetch_json(f"{self.url}/critter/{name}/")
+
+    async def get_fossil(self, name: str) -> dict:
+        return await self._fetch_json(f"{self.url}/fossil/{name}/")
--- nookipedia/nookipedia.py
+++ nookipedia/nookipedia.py
@@ -1,10 +1,7 @@
 from typing import Optional
 
-import aiohttp
-
+from nookipedia.api import API
 from nookipedia.models import Critter, Fossil, Villager
-
-API = "https://nookipedia.com/api"
 
 
 def is_valid(data: dict) -> bool:
@@ -15,37 +12,22 @@ def is_valid(data: dict) -> bool:
 
 class Nookipedia:
     def __init__(self, api_key: str):
-        self.api_key = api_key
-
-    async def _fetch_json(self, url: str) -> dict:
-        headers = {"X-API-KEY": self.api_key}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url=url, headers=headers) as response:
-                return await response.json()
-
-    async def get_villager_raw(self, name: str) -> dict:
-        return await self._fetch_json(f"{API}/villager/{name}/")
-
-    async def get_critter_raw(self, name: str) -> dict:
-        return await self._fetch_json(f"{API}/critter/{name}/")
-
-    async def get_fossil_raw(self, name: str) -> dict:
-        return await self._fetch_json(f"{API}/fossil/{name}/")
+        self.api = API(api_key)
 
     async def get_villager(self, name: str) -> Optional[Villager]:
-        data = await self.get_villager_raw(name)
+        data = await self.api.get_villager(name)
         if not is_valid(data):
             return None
         return Villager(data=data)
 
     async def get_critter(self, name: str) -> Optional[Critter]:
-        data = await self.get_critter_raw(name)
+        data = await self.api.get_critter(name)
         if not is_valid(data):
             return None
         return Critter(data=data)
 
     async def get_fossil(self, name: str) -> Optional[Fossil]:
-        data = await self.get_fossil_raw(name)
+        data = await self.api.get_fossil(name)
         if not is_valid(data):
             return None
         return Fossil(data=data)
