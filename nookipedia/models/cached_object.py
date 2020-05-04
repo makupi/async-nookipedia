b0aaca64953cd1728b7d39b7855f30d97a3ef708
makubob <makupi@icloud.com>
2020-05-04 05:47:20 +0200

    Adds docstrings to CachedObject

--- nookipedia/models/cached_object.py
+++ nookipedia/models/cached_object.py
@@ -1,4 +1,14 @@
 class CachedObject:
+    """
+    Base Object with nookipedias caching/update parameters
+
+    :param data: JSON from API endpoint as dict.
+
+    :var self.updated: time when the endpoint was last updated
+    :var self.cached: time when the endpoint was last cached
+    :var self.api_expire_cache: time when the cache expires
+    """
+
     def __init__(self, data: dict):
         self.updated = data.get("updated")
         self.cached = data.get("cached")
