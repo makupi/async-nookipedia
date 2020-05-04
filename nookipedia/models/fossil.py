0fdb3a0b85ed95184f0da94c5a18fcd0c6e671b1
makubob <makupi@icloud.com>
2020-05-04 05:27:18 +0200

    Updates docstrings fossil

--- nookipedia/models/fossil.py
+++ nookipedia/models/fossil.py
@@ -2,6 +2,21 @@ from .cached_object import CachedObject
 
 
 class Fossil(CachedObject):
+    """
+        Object representing a Critter.
+
+        :param data: JSON from API endpoint as dict.
+
+        :var self.name: name of the fossil
+        :var self.image: url to the image of the fossil
+        :var self.scientific_name:
+        :var self.sections:
+        :var self.period:
+        :var self.length:
+        :var self.price:
+        :var self.link: url to the nookipedia page of the fossil
+    """
+
     def __init__(self, data: dict):
         super().__init__(data)
         self.name = data.get("name")
