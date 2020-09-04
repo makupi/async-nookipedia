c95c80987eb0c4f0cc3a697d96cf40381fa29c2b
makubob <makupi@icloud.com>
2020-09-04 13:10:55 +0200

    Add logging handler

--- nookipedia/__init__.py
+++ nookipedia/__init__.py
@@ -1,5 +1,7 @@
 from importlib_metadata import version
+import logging
 
 from .nookipedia import Nookipedia
 
-__version__ = version('async-nookipedia')
+__version__ = version("async-nookipedia")
+logging.getLogger("async-nookipedia").addHandler(logging.NullHandler())
