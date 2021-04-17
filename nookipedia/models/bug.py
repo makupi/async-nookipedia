eabb4fd113570baf0af2e166221a2518caa687f7
makubob <makupi@icloud.com>
2021-04-17 18:33:05 +0200

    Move to common critter model

--- nookipedia/models/bug.py
+++ nookipedia/models/bug.py
@@ -1,31 +1,5 @@
-import typing as _t
-from pydantic import BaseModel
+from nookipedia.models.common import CommonCritter
 
 
-class Availability(BaseModel):
-    months: str
-    time: str
-
-
-class Hemisphere(BaseModel):
-    availability_array: _t.List[Availability]
-    times_by_month: _t.Dict[str, str]
-    months: str
-    months_array: _t.List[int]
-
-
-class Bug(BaseModel):
-    url: str
-    name: str
-    image_url: str
-    catchphrases: _t.List[str]
-    time: _t.Optional[str]
-    location: str
-    rarity: str
-    total_catch: int
-    sell_nook: int
+class Bug(CommonCritter):
     sell_flick: int
-    tank_width: int
-    tank_length: int
-    north: Hemisphere
-    south: Hemisphere
